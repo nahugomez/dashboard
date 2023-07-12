f3f9cef9db8546a1e0ae600b6bf8a9d14049c1dd
Nahuel Gomez <nahuelgomezs@outlook.com>
2023-07-11 21:23:07 -0300

    Sidebar Modified (Items & Styles)

--- src/global/ApplicationSidebar.jsx
+++ src/global/ApplicationSidebar.jsx
@@ -3,6 +3,8 @@ import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import HomeIcon from "@mui/icons-material/Home";
 import SettingsIcon from "@mui/icons-material/Settings";
 import InventoryIcon from "@mui/icons-material/Inventory";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 
 const ApplicationSidebar = () => {
     return (
@@ -15,6 +17,17 @@ const ApplicationSidebar = () => {
                     <MenuItem>Almacen</MenuItem>
                 </SubMenu>
                 <SubMenu icon={<InventoryIcon />} label="Recursos">
+                    <MenuItem>Personal</MenuItem>
+                    <MenuItem>Inventario</MenuItem>
+                    <MenuItem>Stock</MenuItem>
+                    <MenuItem>Proveedores</MenuItem>
+                </SubMenu>
+                <SubMenu icon={<AssignmentIcon />} label="Tareas">
+                    <MenuItem>Personal</MenuItem>
+                    <MenuItem>Personas</MenuItem>
+                    <MenuItem>Almacen</MenuItem>
+                </SubMenu>
+                <SubMenu icon={<InsertDriveFileIcon  />} label="Contratos">
                     <MenuItem>Personal</MenuItem>
                     <MenuItem>Inventario</MenuItem>
                     <MenuItem>Stock</MenuItem>
