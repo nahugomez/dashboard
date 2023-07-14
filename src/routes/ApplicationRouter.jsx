f7612fcea10fa11d28fa8483cffa121c3dc65345
Nahuel Gomez Suarez <nahuelgomezs@outlook.com>
2023-07-14 11:02:23 -0300

    Not Found Routing

--- src/routes/ApplicationRouter.jsx
+++ src/routes/ApplicationRouter.jsx
@@ -3,15 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Init from "../pages/Init";
 import Resources from "../pages/Resources";
 import Configs from "../pages/Configs";
+import NotFound from "../pages/NotFound";
 
 const ApplicationRouter = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Init />} />
-            <Route path="/resources" element={<Resources />} />
-            <Route path="/configs" element={<Configs />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<Init />} />
+      <Route path="/resources" element={<Resources />} />
+      <Route path="/configs" element={<Configs />} />
+    </Routes>
+  );
 };
 
 export default ApplicationRouter;
