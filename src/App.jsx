9645151d482f7a7a65ecc654510e14d6b048b089
Nahuel Gomez Suarez <nahuelgomezs@outlook.com>
2023-08-04 11:09:41 -0300

    SidebarProvider now in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,29 @@ import { BrowserRouter } from "react-router-dom";
 import ApplicationRouter from "./routes/ApplicationRouter";
 import ApplicationSidebar from "./global/ApplicationSidebar";
 import Navbar from "./global/Navbar";
-import SidebarProvider from "./context/SidebarContext";
 import { Box, CssBaseline } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
-    <SidebarProvider>
-      <Box sx={{ backgroundColor: "#F3F6F9" }}>
-        <BrowserRouter>
-          <CssBaseline />
-          <Navbar />
-          <Box display={"flex"} sx={{ minHeight: "calc(100vh - 4rem)" }}>
-            <ApplicationSidebar />
-            <Box
-              padding={"1rem"}
-              margin={"0.5rem"}
-              borderRadius={"1rem"}
-              width={"100%"}
-              sx={{ backgroundColor: "#FFF", maxHeight: "calc(100vh - 5rem)", overflowY: "scroll" }}
-            >
-              <ApplicationRouter />
-            </Box>
+    <Box sx={{ backgroundColor: "#F3F6F9" }}>
+      <BrowserRouter>
+        <CssBaseline />
+        <Navbar />
+        <Box display={"flex"} sx={{ minHeight: "calc(100vh - 4rem)" }}>
+          <ApplicationSidebar />
+          <Box
+            padding={"1rem"}
+            margin={"0.5rem"}
+            borderRadius={"1rem"}
+            width={"100%"}
+            sx={{ backgroundColor: "#FFF", maxHeight: "calc(100vh - 5rem)", overflowY: "scroll" }}
+          >
+            <ApplicationRouter />
           </Box>
-        </BrowserRouter>
-      </Box>
-    </SidebarProvider>
+        </Box>
+      </BrowserRouter>
+    </Box>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import SidebarProvider from "./context/SidebarContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <App />
+    <SidebarProvider>
+      <App />
+    </SidebarProvider>
   </AuthProvider>
 );
